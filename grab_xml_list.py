e0def37d2719efdcfb7f0438cb5bd4516b802455
Patrick Talley <ptalley@localhost.localdomain>
2017-08-23 14:28:01 -0400

    added comments

--- grab_xml_list.py
+++ grab_xml_list.py
@@ -6,6 +6,8 @@ reponse, content = h.request('https://myanimelist.net/malappinfo.php?u=shaliber&
 
 tree = etree.fromstring(content)
 
+# Series status = 1 means currently airing
+# My status = 1 means currently watching
 title = tree.xpath("//anime[./series_status/text()='1' and ./my_status/text()='1']//series_title/text()")
 series_ids = tree.xpath("//anime[./series_status/text()='1' and ./my_status/text()='1']//series_animedb_id/text()")
 urls = []
@@ -15,13 +17,3 @@ for s_id in series_ids:
 
 my_dict = dict(zip(title, urls))
 print(my_dict)
-# for elem in tree.iter():
-#     air_status = tree.xpath("//anime//series_status")
-#     my_status = tree.xpath("//anime//my_status")
-#     if air_status == "1" and my_status == "1":
-#         print("test")
-#         series_title = tree.xpath("//anime//series_title/text()")
-#         series_id  = tree.xpath("//anime//series_animedb_id/text()")
-#         link = "https://myanimelist.net/anime/%s" % series_id
-#         airing_dict = {"title": series_title,
-#                         "link": link}
